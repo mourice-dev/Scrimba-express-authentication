ae75abf59abde7d556ba467d92e9a3eee2e4ef1c
Nshuti Maurice <nshutikope@gmail.com>
2025-12-05 17:54:11 +0200

    ft/auth-route

---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import { registerUser } from "../controllers/authController.js";
+import express from "express";
+
+export const authRouter = express.Router();
+
+authRouter.post("/register", registerUser);
