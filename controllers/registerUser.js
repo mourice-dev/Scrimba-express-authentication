26e2fcafa43cf87f573168bcc2618c220952be82
Nshuti Maurice <nshutikope@gmail.com>
2025-12-08 17:04:09 +0200

    data form validation

--- controllers/registerUser.js
+++ controllers/registerUser.js
@@ -1,5 +1,50 @@
 /** @format */
+import { validator } from 'validator';
 
 export async function registerUser(req, res) {
-  console.log("req.body: ", req.body);
+
+let { name, email, username, password } = req.body;
+
+  /*
+Challenge:
+1. Validate the incoming user data.
+  - Make sure all fields are present.
+  - Remove any whitespace where appropriate.
+  - Use regex /^[a-zA-Z0-9_-]{1,20}$/ to check the username contains only the allowed characters.
+  - Use the Validator package to check the email format is valid.
+
+If fields are not present, the username uses disallowed characters, or the email address is not of a valid format, end the response with a suitable code and send an error object with a suitable message. For example:
+   
+   { error: 'All fields are required.' }
+
+  - Test with console.logs.
+
+hint.md for help!
+*/
+  if (!name || email || username || password) {
+    res.status(400).json({ error: "All fields are required." });
+  }
+  
+    name = name.trim();
+    email = email.trim();
+    username = username.trim();
+
+
+  if (! /^[a-zA-Z0-9_-]{1,20}$/.test(username)) {
+    res.status(400).json({
+      error:
+        "Username must be 1–20 characters, using letters, numbers, _ or -.",
+    });
+    
+  }
+  
+  if (!validator.isEmail(email)) {
+        res.status(400).json({
+          error:
+            "Invalid email"
+        });  
+  
+  }
+
+
 }
