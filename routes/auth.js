e980654eea59f4991d2e82daebd32e6f2871dbad
Nshuti Maurice <nshutikope@gmail.com>
2025-12-08 16:12:24 +0200

    Resize forms

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import { registerUser } from "../controllers/authController.js";
+import { registerUser } from "../controllers/registerUser.js";
 import express from "express";
 
 export const authRouter = express.Router();
